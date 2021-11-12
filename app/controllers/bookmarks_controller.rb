a96cfb102c4c3d3266abafba30ee8164e43979f5
Jan van Heusde <janvheusde@gmail.com>
2021-11-12 11:10:43 -0500

    rials watch heroku

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,2 +1,5 @@
 class BookmarksController < ApplicationController
+  def index
+    @movies = Movie.all
+  end
 end
--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,2 +1,29 @@
 class ListsController < ApplicationController
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @list = List.find(params[:id])
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    @list.save
+
+    # redirect_to restaurant_path(@restaurant)
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
+  end
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
 end
